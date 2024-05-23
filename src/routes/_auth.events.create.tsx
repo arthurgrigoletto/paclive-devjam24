84fca4e76a5ec981bc0179e0156856f5d97ac5ba
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2024-05-23 07:23:20 -0300

    add new create event page

--- src/routes/_auth.events.create.tsx
+++ src/routes/_auth.events.create.tsx
@@ -1,5 +1,45 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { ArrowLeft } from 'lucide-react'
+
+import { EventForm } from '@/components/event-form'
 
 export const Route = createFileRoute('/_auth/events/create')({
-  component: () => <div>Hello /_auth/events/create!</div>,
+  component: CreateEventPage,
 })
+
+const FORM_ID = 'CREATE_EVENT_FORM_ID'
+
+function CreateEventPage() {
+  return (
+    <section className="flex flex-col space-y-8 bg-[#F5F7FA]">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-2">
+          <Link className="inline-flex items-center gap-2" to="/">
+            <ArrowLeft className="size-3" />
+            <span className="text-sm font-normal text-[#181F25] underline">
+              Back to All Events
+            </span>
+          </Link>
+          <h2 className="text-3xl font-bold text-[#181F25]">Add a New Event</h2>
+        </div>
+        <div className="flex items-center gap-6">
+          <Link
+            to="/"
+            className="inline-flex h-12 cursor-pointer items-center gap-2 rounded border border-[#CCD1D9] bg-white px-3 py-2 text-base font-bold shadow-[0_4px_8px_0_rgba(0,0,0,0.1)]"
+          >
+            Cancel
+          </Link>
+          <button
+            form={FORM_ID}
+            type="submit"
+            className="inline-flex h-12 cursor-pointer items-center gap-2 rounded border bg-[#B5082A] p-3 px-3 py-2 text-base font-bold text-white shadow-[0_4px_8px_0_rgba(0,0,0,0.1)]"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+
+      <EventForm id={FORM_ID} />
+    </section>
+  )
+}
--- src/routes/_auth.index.tsx
+++ src/routes/_auth.index.tsx
@@ -1,12 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { Search } from 'lucide-react'
 
 import { getEvents } from '@/api/get-events'
 import { Chart } from '@/components/icons/chart'
 import { Filter } from '@/components/icons/filter'
 import { Plus } from '@/components/icons/plus'
-import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/_auth/')({
   component: DashboardPage,
@@ -25,13 +24,16 @@ function DashboardPage() {
       <div className="flex items-center justify-between">
         <h2 className="text-3xl font-bold text-[#181F25]">Event List</h2>
         <div className="flex items-center gap-8">
-          <button className="inline-flex h-12 cursor-pointer items-center gap-2 rounded-sm border border-[#CCD1D9] bg-white px-3 py-2 text-xs font-bold shadow-[0_4px_8px_0_rgba(0,0,0,0.1)]">
+          <button className="inline-flex h-12 cursor-pointer items-center gap-2 rounded border border-[#CCD1D9] bg-white px-3 py-2 text-base font-bold shadow-[0_4px_8px_0_rgba(0,0,0,0.1)]">
             <Chart className="size-5" />
             Data & Insight
           </button>
-          <Button size="lg" className="inline-flex items-center gap-2">
+          <Link
+            className="inline-flex h-12 cursor-pointer items-center gap-2 rounded border bg-[#B5082A] p-3 px-3 py-2 text-base text-white shadow-[0_4px_8px_0_rgba(0,0,0,0.1)]"
+            to="/events/create"
+          >
             <Plus className="size-5" /> Add New
-          </Button>
+          </Link>
         </div>
       </div>
 
